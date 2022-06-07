92dd5ad4bb2084ea125a8c59b26b30f6ae2ccd92
111084 <111084@w.tmu.edu.tw>
2022-06-07 10:36:26 +0800

    sourcemamp, tree shaking, asset modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/main.js",
+  // 模式 *(切換到 production ，開啟 tree shaking 功能)
   mode: "development",
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -13,7 +15,12 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      //asset loader
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
@@ -22,5 +29,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
+    new MiniCssExtractPlugin({
+      filename: "index.[fullhash].css",
+    }),
   ],
+  devtool: "source-map",
 };
